523ac3e02a6047ef9954dd7c7d2ed0d404321e22
Mikhail Borisov <mishaborisov2013@gmail.com>
2022-10-01 20:04:34 +0300

    Initial Commit

---
+++ GradientEffect/GradientEffect.h
@@ -0,0 +1,18 @@
+//
+//  GradientEffect.h
+//  GradientEffect
+//
+//  Created by Mikhail Borisov on 01.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GradientEffect.
+FOUNDATION_EXPORT double GradientEffectVersionNumber;
+
+//! Project version string for GradientEffect.
+FOUNDATION_EXPORT const unsigned char GradientEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientEffect/PublicHeader.h>
+
+
